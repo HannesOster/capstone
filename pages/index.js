63f8166a737d3474d8ae2f10994d4d1dc195afcf
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-07-21 08:58:27 +0200

    add header to menu

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,6 @@ export default function Home() {
   return (
     <>
       <Header />
-
-      <StyledLink href="/list-page">List</StyledLink>
     </>
   );
 }
--- pages/menu.js
+++ pages/menu.js
@@ -1,3 +1,5 @@
+import Header from "../components/Header/Header";
+
 export default function Menu() {
-  return <div></div>;
+  return <Header />;
 }
