4b17f6a1372094e79b3c64e21a8d38f4d59934ad
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-07-24 12:46:38 +0200

    add cancel button functionality

--- components/Buttons/CancelButton.js
+++ components/Buttons/CancelButton.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
-import { FiSave } from "react-icons/fi";
+import Link from "next/link";
 
+const RedButton = styled(Link)`
+  color: red;
+  background-color: white;
+  border: 2px red solid;
+  border-radius: 3%;
+  padding: 5px 20px;
+`;
 export default function CancelButton() {
-  const RedButton = styled.button`
-    color: red;
-    background-color: white;
-    border: 2px red solid;
-    border-radius: 3%;
-    padding: 5px 20px;
-  `;
-  return <RedButton>Abbrechen</RedButton>;
+  return <RedButton href="/">Abbrechen</RedButton>;
 }
