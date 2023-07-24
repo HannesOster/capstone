05c86ac7de1b0a64ccd3f2cab987a46838ed5ff9
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-07-24 15:45:42 +0200

    added invitation

---
+++ pages/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Invitation = styled.h1`
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+export { Invitation };
