b2fb62e22a484e8c4e44c7f4285ae9b820f0e755
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-07-25 19:01:58 +0200

    add general menu styles

--- pages/menu.js
+++ pages/menu.js
@@ -1,5 +1,19 @@
 import Header from "../components/Header/Header";
+import { MenuLink, MenuContainer } from "../components/Buttons/styles";
 
 export default function Menu() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <MenuContainer>
+        <MenuLink href="#">Kunden Hinzufügen</MenuLink>
+        <MenuLink href="#">Kunden Hinzufügen</MenuLink>
+        <MenuLink href="#">Kunden Hinzufügen</MenuLink>
+        <MenuLink href="#">Kunden Hinzufügen</MenuLink>
+        <MenuLink href="#">Kunden Hinzufügen</MenuLink>
+        <MenuLink href="#">Kunden Hinzufügen</MenuLink>
+        <MenuLink href="#">Kunden Hinzufügen</MenuLink>
+      </MenuContainer>
+    </>
+  );
 }
