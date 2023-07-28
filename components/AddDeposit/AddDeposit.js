02a3324b5469f488d14d87dfbe71e22b22e46e19
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-07-28 22:34:01 +0200

    style fix and customer stock display

--- components/AddDeposit/AddDeposit.js
+++ components/AddDeposit/AddDeposit.js
@@ -1,5 +1,13 @@
-import { AddDepositHeading } from "./styles";
+import { AddDepositHeading, CustomerStock, StockContainer } from "./styles";
 
 export default function AddDeposit({ customer }) {
-  return <AddDepositHeading>{customer.name}</AddDepositHeading>;
+  return (
+    <>
+      <AddDepositHeading>{customer.name}</AddDepositHeading>
+      <StockContainer>
+        <CustomerStock>Kisten: {customer.boxes}</CustomerStock>
+        <CustomerStock>Eimer: {customer.buckets}</CustomerStock>
+      </StockContainer>
+    </>
+  );
 }
--- components/AddDeposit/styles.js
+++ components/AddDeposit/styles.js
@@ -3,5 +3,14 @@ import styled from "styled-components";
 const AddDepositHeading = styled.h1`
   margin-top: 100px;
 `;
+const StockContainer = styled.section`
+  display: flex;
+  justify-content: space-around;
+`;
+const CustomerStock = styled.p`
+  background-color: ${(props) => props.theme.fourth};
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
 
-export { AddDepositHeading };
+export { AddDepositHeading, CustomerStock, StockContainer };
