5b1a13bf3e3d0ca5721ccbdf3b199937a0139951
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-08-03 16:18:20 +0200

    add dynamic import

--- pages/menu/customer-map/index.js
+++ pages/menu/customer-map/index.js
@@ -1,6 +1,8 @@
 import LoadingSpinner from "../../../components/LoadingSpinner/LoadingSpinner";
-import Map from "../../../components/Map";
+import dynamic from "next/dynamic";
 import useSWR from "swr";
+
+const Map = dynamic(() => import("../../../components/Map"), { ssr: false });
 
 export default function CustomerMap() {
   const { data, error, isLoading } = useSWR("/api/customers", {
