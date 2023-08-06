b260e4237dc8e2749f258d297b1d83e8e6f437e6
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-08-06 16:25:38 +0200

    add marker styles

--- components/Map/index.js
+++ components/Map/index.js
@@ -16,6 +16,26 @@ const StyledMapContainer = styled(MapContainer)`
 
 const goldIcon = new L.Icon({
   iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+const greenIcon = new L.Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+const redIcon = new L.Icon({
+  iconUrl:
     "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
   shadowUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
@@ -24,7 +44,6 @@ const goldIcon = new L.Icon({
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
 });
-
 export default function Map({ markers }) {
   return (
     <StyledMapContainer center={[51.601, 6.659]} zoom={9} scrollWheelZoom>
