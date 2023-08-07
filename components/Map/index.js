a2302e069b1a320761bfafa1d3c9a6ea1d8d5bec
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-08-07 12:43:59 +0200

    changed box count icon to count

--- components/Map/index.js
+++ components/Map/index.js
@@ -6,13 +6,6 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import "leaflet-defaulticon-compatibility";
 import LocationMarker from "./LocationMarker";
 import { CustomerStock } from "../AddDeposit/styles";
-import { IconContext } from "react-icons";
-import {
-  PiCellSignalFullFill,
-  PiCellSignalHighFill,
-  PiCellSignalMediumFill,
-  PiCellSignalLowFill,
-} from "react-icons/pi";
 import { renderToStaticMarkup } from "react-dom/server";
 import CustomerNavigation from "../CustomerNavigation/CustomerNavigation";
 
@@ -22,6 +15,7 @@ const StyledMapContainer = styled(MapContainer)`
   margin-top: 90px;
   z-index: 0;
 `;
+
 const StyledLegend = styled.div`
   position: fixed;
   top: 5px;
@@ -40,6 +34,7 @@ const StyledPopup = styled(Popup)`
   background-color: ${(props) => props.theme.third};
   z-index: 1005;
 `;
+
 const yellowMarker = new L.Icon({
   iconUrl:
     "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png",
@@ -50,6 +45,7 @@ const yellowMarker = new L.Icon({
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
 });
+
 const greenMarker = new L.Icon({
   iconUrl:
     "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
@@ -60,6 +56,7 @@ const greenMarker = new L.Icon({
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
 });
+
 const redMarker = new L.Icon({
   iconUrl:
     "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
@@ -70,6 +67,7 @@ const redMarker = new L.Icon({
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
 });
+
 const blackMarker = new L.Icon({
   iconUrl:
     "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png",
@@ -81,88 +79,34 @@ const blackMarker = new L.Icon({
   shadowSize: [41, 41],
 });
 
-const boxCountIcon20 = new L.divIcon({
-  html: renderToStaticMarkup(
-    <IconContext.Provider
-      value={{
-        color: "black",
-        size: "25px",
-      }}
-    >
-      <PiCellSignalFullFill
-        style={{ color: "#D31119", transform: "translate(-4px,-55px)" }}
-      />{" "}
-    </IconContext.Provider>
-  ),
-  iconSize: [0, 0],
-  iconAnchor: [10, 10],
-});
+function createBoxCountIcon(boxCount) {
+  return new L.divIcon({
+    html: renderToStaticMarkup(
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "15px",
+          height: "15px",
+          borderRadius: "50%",
+          backgroundColor: "white",
 
-const boxCountIcon15 = new L.divIcon({
-  html: renderToStaticMarkup(
-    <IconContext.Provider
-      value={{
-        color: "black",
-        size: "25px",
-      }}
-    >
-      <PiCellSignalHighFill
-        style={{ color: "#D31119", transform: "translate(-4px,-55px)" }}
-      />{" "}
-    </IconContext.Provider>
-  ),
-  iconSize: [0, 0],
-  iconAnchor: [10, 10],
-});
-const boxCountIcon10 = new L.divIcon({
-  html: renderToStaticMarkup(
-    <IconContext.Provider
-      value={{
-        color: "black",
-        size: "25px",
-      }}
-    >
-      <PiCellSignalMediumFill
-        style={{ color: "#D31119", transform: "translate(-4px,-55px)" }}
-      />{" "}
-    </IconContext.Provider>
-  ),
-  iconSize: [0, 0],
-  iconAnchor: [10, 10],
-});
-const boxCountIcon5 = new L.divIcon({
-  html: renderToStaticMarkup(
-    <IconContext.Provider
-      value={{
-        color: "black",
-        size: "25px",
-      }}
-    >
-      <PiCellSignalLowFill
-        style={{ color: "#D31119", transform: "translate(-4px,-55px)" }}
-      />{" "}
-    </IconContext.Provider>
-  ),
-  iconSize: [0, 0],
-  iconAnchor: [10, 10],
-});
+          fontSize: "8px",
+          transform: "translate(4px,4px)",
+        }}
+      >
+        {boxCount}
+      </div>
+    ),
+    iconSize: [0, 0],
+    iconAnchor: [12, 41],
+  });
+}
 
 export default function Map({ markers }) {
   return (
     <>
-      <StyledLegend>
-        <PiCellSignalLowFill style={{ color: "#D31119" }} />
-        <span>Bis 5 Kisten</span>
-        <br />
-        <PiCellSignalMediumFill style={{ color: "#D31119" }} />
-        <span>Bis 10 Kisten</span>
-        <br />
-        <PiCellSignalHighFill style={{ color: "#D31119" }} />
-        <span>Bis 15 Kisten</span>
-        <br />
-        <PiCellSignalFullFill style={{ color: "#D31119" }} />
-        <span>Über 15 Kisten</span>
-      </StyledLegend>
       <StyledMapContainer center={[51.601, 6.659]} zoom={9} scrollWheelZoom>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -174,60 +118,43 @@ export default function Map({ markers }) {
             return null;
           }
           return (
-            <Marker
-              key={marker.id}
-              position={[marker.lat, marker.long]}
-              icon={
-                isNaN(marker.days)
-                  ? blackMarker
-                  : marker.days > 30
-                  ? redMarker
-                  : marker.days > 10
-                  ? yellowMarker
-                  : greenMarker
-              }
-            >
-              <StyledPopup>
-                <h2>{marker.name}</h2>
-                <CustomerStock>Kisten: {marker.boxes}</CustomerStock>
-                <CustomerStock>
-                  {isNaN(marker.days)
-                    ? "Noch keine Lieferung"
-                    : marker.days === 0
-                    ? "Letzte Lieferung heute"
-                    : marker.days === 1
-                    ? `Letzte Lieferung gestern`
-                    : `Letzte Lieferung vor ${marker.days} Tagen`}
-                </CustomerStock>
-                <CustomerNavigation customer={marker.customer} />
-              </StyledPopup>
+            <>
+              <Marker
+                key={marker.id}
+                position={[marker.lat, marker.long]}
+                icon={
+                  isNaN(marker.days)
+                    ? blackMarker
+                    : marker.days > 30
+                    ? redMarker
+                    : marker.days > 10
+                    ? yellowMarker
+                    : greenMarker
+                }
+              >
+                <StyledPopup>
+                  <h2>{marker.name}</h2>
+                  <CustomerStock>Kisten: {marker.boxes}</CustomerStock>
+                  <CustomerStock>
+                    {isNaN(marker.days)
+                      ? "Noch keine Lieferung"
+                      : marker.days === 0
+                      ? "Letzte Lieferung heute"
+                      : marker.days === 1
+                      ? `Letzte Lieferung gestern`
+                      : `Letzte Lieferung vor ${marker.days} Tagen`}
+                  </CustomerStock>
+                  <CustomerNavigation customer={marker.customer} />
+                </StyledPopup>
+              </Marker>
 
               {marker.boxes > 0 && (
-                <>
-                  {marker.boxes <= 5 ? (
-                    <Marker
-                      position={[marker.lat, marker.long]}
-                      icon={boxCountIcon5}
-                    />
-                  ) : marker.boxes <= 10 ? (
-                    <Marker
-                      position={[marker.lat, marker.long]}
-                      icon={boxCountIcon10}
-                    />
-                  ) : marker.boxes <= 15 ? (
-                    <Marker
-                      position={[marker.lat, marker.long]}
-                      icon={boxCountIcon15}
-                    />
-                  ) : (
-                    <Marker
-                      position={[marker.lat, marker.long]}
-                      icon={boxCountIcon20}
-                    />
-                  )}
-                </>
+                <Marker
+                  position={[marker.lat, marker.long]}
+                  icon={createBoxCountIcon(marker.boxes)}
+                />
               )}
-            </Marker>
+            </>
           );
         })}
         <LocationMarker />
