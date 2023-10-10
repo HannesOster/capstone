7b903e2e83cefbdfaa9fe41af7986644a0f07206
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-10-10 11:37:16 +0200

    changed table style

--- components/List/styles.js
+++ components/List/styles.js
@@ -1,28 +1,27 @@
 import { BiListMinus, BiListPlus } from "react-icons/bi";
-import { ImCircleDown, ImCircleUp } from "react-icons/im";
+import { ImCircleDown } from "react-icons/im";
 import { RxCheckCircled, RxCrossCircled } from "react-icons/rx";
 import styled from "styled-components";
 
 export const StyledTableHeading = styled.th`
   background-color: ${(props) =>
-    props.active ? props.theme.third : props.theme.fourth};
-  color: black;
+    props.active ? "Whitesmoke" : props.theme.primary};
+  color: ${(props) => (props.active ? "black" : "Whitesmoke")};
   text-align: center;
   font-weight: 600;
   font-size: 15px;
-  border-bottom: 2px solid ${(props) => props.theme.fourth};
   transition: all 0.4s ease-out;
+  border-bottom: ${(props) => (props.active ? "2px solid Gainsboro" : "none")};
   &:first-child {
     text-align: left;
   }
 `;
 export const StyledTableHeadingAttachments = styled.th`
   background-color: ${(props) =>
-    props.active ? props.theme.third : props.theme.fourth};
-  color: black;
+    props.active ? "Whitesmoke" : props.theme.primary};
+  color: ${(props) => (props.active ? "black" : "Whitesmoke")};
   text-align: center;
   font-weight: 600;
-  border-bottom: ${(props) => (props.active ? "2px solid black" : "none")};
   transition: all 0.4s ease-out;
   font-size: 12px;
   &:first-child {
@@ -31,7 +30,7 @@ export const StyledTableHeadingAttachments = styled.th`
 `;
 export const StyledTableCell = styled.td`
   width: ${(props) => (props.active ? "20" : "25%")};
-  border: 2px solid ${(props) => props.theme.fourth};
+  border: 2px solid Gainsboro;
   text-align: center;
   font-weight: 550;
   &:first-child {
@@ -40,17 +39,18 @@ export const StyledTableCell = styled.td`
 `;
 export const StyledTable = styled.table`
   border-collapse: collapse;
+  border: none;
   width: 100%;
   margin-bottom: 70px;
 `;
 export const StyledTableRow = styled.tr`
   height: 50px;
   &:nth-child(odd) {
-    background-color: ${(props) => props.theme.fifth};
+    background-color: Whitesmoke;
   }
 `;
 export const HeadingTableRow = styled.tr`
-  height: 50px;
+  height: 60px;
   position: sticky;
   top: 0;
   left: 0;
@@ -77,13 +77,15 @@ export const ReducedListIcon = styled(BiListMinus)`
 
 export const CheckIcon = styled(RxCheckCircled)`
   color: ${(props) => props.theme.success};
+  font-size: 20px;
 `;
 
 export const CrossIcon = styled(RxCrossCircled)`
   color: ${(props) => props.theme.danger};
+  font-size: 20px;
 `;
 
 export const DownIcon = styled(ImCircleDown)`
   transform: translate(3px, 2px);
-  color: ${(props) => props.theme.fourth};
+  color: black;
 `;
